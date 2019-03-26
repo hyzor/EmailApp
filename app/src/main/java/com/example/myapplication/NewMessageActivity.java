4beec91139a257cbab5e31d71f04db6190530863
Jesper Hansson Falkenby <jesper.hansson.f@gmail.com>
2019-03-26 21:18:24 +0100

    Fix structure and change project name.

--- app/src/main/java/com/example/myapplication/NewMessageActivity.java
+++ app/src/main/java/com/example/myapplication/NewMessageActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.emailapp;
 
 import android.app.Activity;
 import android.content.Intent;
--- app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.emailapp;
 
 import org.junit.Test;
 
